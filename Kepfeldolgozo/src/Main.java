fc9ece8ed182565f831c16be07e36ed2268b5bce
hunti <User@User-PC>
2012-04-08 23:21:50 +0200

    Na majd most menni fog!:)

--- Kepfeldolgozo/src/Main.java
+++ Kepfeldolgozo/src/Main.java
@@ -7,6 +7,9 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world! NyanCat");
 		System.out.println("Tezstelek");
+		
+		int valami = 0;
+		if (valami == 0) System.out.println("Ez egy nagy nulla! o.O");
 		// TODO Auto-generated method stub
 
 	}
