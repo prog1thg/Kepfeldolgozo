eee53c9257d3667420fe7db15b55e59bd765966f
hunti <User@User-PC>
2012-04-08 22:57:54 +0200

    Második felvonás!

--- Kepfeldolgozo/src/Main.java
+++ Kepfeldolgozo/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		System.out.println("Hello world! NyanCat");
 		// TODO Auto-generated method stub
 
 	}
