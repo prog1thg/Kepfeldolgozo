463b335014bfbe36b3143b207097779ea2a4f7f6
Zoo_Lee <zzllee@citromail.hu>
2012-04-10 02:56:53 +0200

    Az eddigi megbeszélések alapján megírtam a tmpImage osztályt, még nem teszteltem, de elvileg jó.

--- Kepfeldolgozo/src/tmpImage.java
+++ Kepfeldolgozo/src/tmpImage.java
@@ -1,8 +1,28 @@
+import java.awt.Color;
 
 public class tmpImage {
-	tmpImage() {
-		int r;
-		int g;
-		int b;
+	private int width;
+	private int height;
+	private Color[] map = new Color[height*width];
+	
+	public void setColor(int i, int j, Color szin){
+		this.map[i][j]=szin;
+	}
+	
+	public Color getColor(int i, int j){
+		return map[i][j];
+	}
+	
+	public int getWidth(){
+		return width;
+	}
+	
+	public int getHeight(){
+		return length;
+	}
+	
+	public tmpImage(int magas, int szeles){
+		this.width=szeles;
+		this.height=magas;
 	}
 }
