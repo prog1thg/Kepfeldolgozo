caa513b7778c7e03730bad126598714de6d20d30
prog1thg <uvegla@hotmail.com>
2012-04-14 17:56:42 +0200

    Main.java update fix

--- Kepfeldolgozo/src/Main.java
+++ Kepfeldolgozo/src/Main.java
@@ -1,14 +1,43 @@
+import parser.CommandLineParser;
+import readers.BmpReader;
+import readers.ImageReader;
+import readers.PpmReader;
+import tmpimage.TmpImage;
+import writers.ImageWriter;
+import writers.PpmWriter;
+
+/**
+ * Image Manipulation Software (C) 2012
+ * 
+ * @author dzdeni
+ * @author Tomi
+ * @author uvegla
+ * @author Zoo_Lee
+ *
+ */
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		System.out.println("Hello world! NyanCat");
-		System.out.println("Commit küldése");
-		System.out.println("Commit küldése 1234 - uvegla");
-		// TODO Auto-generated method stub
+	private static ImageReader reader; 
+	private static ImageWriter writer;
+	private static CommandLineParser cmd;
+	private static TmpImage tmp;	
+	
+	public static void main(String args[]) {
+		cmd = new parser.CommandLineParser(args);
+		
+		reader = ReaderFactory.getReader(cmd.ext, cmd.filein);
+		
+		tmp = reader.convertToTmpImage();
+
+		/** Effects && Transformation should go here */
+		
+		writer = WriterFactory.getWriter(cmd.ext, tmp, cmd.fileout);
+
+		writer.writeToFile();
+
+
+		System.out.println(cmd.filein +" converted to "+ cmd.fileout +".\n\nIt was so easy!\n");
 
 	}
 
